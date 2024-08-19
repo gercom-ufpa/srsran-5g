a0751b5af0973a281466e59c9c9366c4743ec448
Murilo Silva <murilosilvavmt@gmail.com>
2024-08-19 11:04:37 -0300

    fixes

--- helm-chart/scripts/entrypoint.sh
+++ helm-chart/scripts/entrypoint.sh
@@ -8,21 +8,26 @@ then
         exit
 fi
 
+ip_check="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
+
 if [[ ! -z "$AMF_HOSTNAME" ]] ; then 
-    export AMF_ADDR="$(host -4 $AMF_HOSTNAME | awk '/has.*address/{print $NF; exit}')"
+    if [[ ${AMF_HOSTNAME} =~ ${ip_check} ]] ; then
+        export AMF_ADDR=${AMF_HOSTNAME} ## is a IP format
+    else
+        export AMF_ADDR="$(host -4 $AMF_HOSTNAME | awk '/has.*address/{print $NF; exit}')" ## is a hostname format -> convert to ipv4
+    fi
 fi
 
 if [[ -z "${AMF_BIND_ADDR}" ]] ; then
     export AMF_BIND_ADDR="$(ip addr show $AMF_BIND_INTERFACE | grep -Po 'inet \K[\d.]+')"
 fi
 
-ip_check="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
 
 if [[ ! -z "$RIC_HOSTNAME" ]] ; then
     if [[ ${RIC_HOSTNAME} =~ ${ip_check} ]] ; then
         export RIC_ADDR=${RIC_HOSTNAME} ## is a IP format
     else
-        export RIC_ADDR="$(host -4 $RIC_HOSTNAME | awk '/has.*address/{print $NF; exit}')" ## is a hostname format
+        export RIC_ADDR="$(host -4 $RIC_HOSTNAME | awk '/has.*address/{print $NF; exit}')" ## is a hostname format -> convert to ipv4
     fi
 fi
 
